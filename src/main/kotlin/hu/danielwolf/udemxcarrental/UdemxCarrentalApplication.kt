b2e424300aea71dc97127f0276c7fcfd2387ebde
Wolf Dániel <wolf.dani9@gmail.com>
2023-05-31 14:03:05 +0200

    Initial commit

---
+++ src/main/kotlin/hu/danielwolf/udemxcarrental/UdemxCarrentalApplication.kt
@@ -0,0 +1,11 @@
+package hu.danielwolf.udemxcarrental
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UdemxCarrentalApplication
+
+fun main(args: Array<String>) {
+	runApplication<UdemxCarrentalApplication>(*args)
+}
---
+++ src/test/kotlin/hu/danielwolf/udemxcarrental/UdemxCarrentalApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.danielwolf.udemxcarrental
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UdemxCarrentalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
