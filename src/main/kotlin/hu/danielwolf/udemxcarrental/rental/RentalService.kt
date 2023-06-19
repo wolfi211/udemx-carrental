21759a92fa30073660ab0af591c3ff73a0957337
Wolf Dániel <wolf.dani9@gmail.com>
2023-06-19 22:18:48 +0200

    refactored SearchRentalForm to SearchModel for better understanding

--- src/main/kotlin/hu/danielwolf/udemxcarrental/rental/RentalService.kt
+++ src/main/kotlin/hu/danielwolf/udemxcarrental/rental/RentalService.kt
@@ -1,6 +1,6 @@
 package hu.danielwolf.udemxcarrental.rental
 
-import hu.danielwolf.udemxcarrental.SearchRentalForm
+import hu.danielwolf.udemxcarrental.models.SearchModel
 import hu.danielwolf.udemxcarrental.car.CarEntity
 import hu.danielwolf.udemxcarrental.car.CarService
 import org.springframework.stereotype.Service
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service
 @Service
 class RentalService(val rentalRepository: RentalRepository, val carService: CarService) {
 
-    fun getCarsByDate(searchRentalForm: SearchRentalForm): List<CarEntity> {
+    fun getCarsByDate(searchRentalForm: SearchModel): List<CarEntity> {
         val rentals = rentalRepository.findAll()
         val validCars = mutableListOf<CarEntity>()
         for(rental : RentalEntity in rentals) {
@@ -19,10 +19,10 @@ class RentalService(val rentalRepository: RentalRepository, val carService: CarS
         return validCars
     }
 
-    fun isRentedAtDate(rental: RentalEntity, searchRentalForm: SearchRentalForm): Boolean =
-        (rental.dateStart < searchRentalForm.dateStart && rental.dateEnd > searchRentalForm.dateEnd) ||
-        (rental.dateStart > searchRentalForm.dateStart && rental.dateEnd < searchRentalForm.dateEnd) ||
-        (rental.dateStart < searchRentalForm.dateStart && rental.dateEnd > searchRentalForm.dateStart) ||
-        (rental.dateStart < searchRentalForm.dateEnd && rental.dateEnd > searchRentalForm.dateEnd)
+    fun isRentedAtDate(rental: RentalEntity, searchRentalForm: SearchModel): Boolean =
+        (rental.dateStart < searchRentalForm.start && rental.dateEnd > searchRentalForm.end) ||
+        (rental.dateStart > searchRentalForm.start && rental.dateEnd < searchRentalForm.end) ||
+        (rental.dateStart < searchRentalForm.start && rental.dateEnd > searchRentalForm.start) ||
+        (rental.dateStart < searchRentalForm.end && rental.dateEnd > searchRentalForm.end)
 
 }
