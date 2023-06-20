d1f5f5bbb54473e8ba400a2c1eaa39e591eaf34d
Wolf Dániel <wolf.dani9@gmail.com>
2023-06-20 22:51:56 +0200

    removed unnecessary functions

--- src/main/kotlin/hu/danielwolf/udemxcarrental/models/DateRangeModel.kt
+++ src/main/kotlin/hu/danielwolf/udemxcarrental/models/DateRangeModel.kt
@@ -7,8 +7,4 @@ import java.time.format.DateTimeFormatter
 data class DateRangeModel (
     var start: LocalDate? = null,
     var end: LocalDate? = null
-) {
-    fun getFormattedStartDate(): String? = start?.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy"))
-
-    fun getFormattedEndDate(): String? = end?.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy"))
-}
+)
