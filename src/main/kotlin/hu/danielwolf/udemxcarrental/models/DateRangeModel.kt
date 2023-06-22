8a5f9630dacb25545d51884743d4de9eeb91d62a
Wolf Dániel <wolf.dani9@gmail.com>
2023-06-22 14:03:34 +0200

    added method to get date difference in days

--- src/main/kotlin/hu/danielwolf/udemxcarrental/models/DateRangeModel.kt
+++ src/main/kotlin/hu/danielwolf/udemxcarrental/models/DateRangeModel.kt
@@ -1,10 +1,12 @@
 package hu.danielwolf.udemxcarrental.models
 
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 
 data class DateRangeModel (
     var start: LocalDate? = null,
     var end: LocalDate? = null
-)
+) {
+    fun getDays(): Long? = start?.until(end, ChronoUnit.DAYS)!! + 1
+}
