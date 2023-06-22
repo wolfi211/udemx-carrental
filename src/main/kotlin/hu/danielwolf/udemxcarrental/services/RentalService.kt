533f8c1a4314e287c8362c5350a895e56a19dbe9
Wolf Dániel <wolf.dani9@gmail.com>
2023-06-22 14:08:57 +0200

    added method to set active status of a car after checking if they have an ongoing rental

--- src/main/kotlin/hu/danielwolf/udemxcarrental/services/RentalService.kt
+++ src/main/kotlin/hu/danielwolf/udemxcarrental/services/RentalService.kt
@@ -8,6 +8,7 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
+import java.time.LocalDate
 
 @Service
 class RentalService(val repository: RentalRepository, val carService: CarService) {
@@ -19,11 +20,11 @@ class RentalService(val repository: RentalRepository, val carService: CarService
     fun getById(id: Long): RentalEntity = repository.findByIdOrNull(id) ?:
         throw ResponseStatusException(HttpStatus.NOT_FOUND)
 
-    fun getCarsByDate(searchRentalForm: DateRangeModel): List<CarEntity> {
+    fun getCarsByDate(dateRange: DateRangeModel): List<CarEntity> {
         val rentals = repository.findAll()
-        val validCars = carService.getAll().toMutableList()
+        val validCars = carService.getAllActive().toMutableList()
         for(rental : RentalEntity in rentals) {
-            if(isRentedAtDate(rental, searchRentalForm)) {
+            if(isRentedAtDate(rental, dateRange)) {
                 validCars.remove(rental.car)
             }
         }
@@ -42,9 +43,21 @@ class RentalService(val repository: RentalRepository, val carService: CarService
         else throw ResponseStatusException(HttpStatus.NOT_FOUND)
     }
 
-    fun isRentedAtDate(rental: RentalEntity, searchRentalForm: DateRangeModel): Boolean =
-        (rental.dateStart < searchRentalForm.start && rental.dateEnd > searchRentalForm.end) ||
-                (rental.dateStart > searchRentalForm.start && rental.dateEnd < searchRentalForm.end) ||
-                (rental.dateStart < searchRentalForm.start && rental.dateEnd > searchRentalForm.start) ||
-                (rental.dateStart < searchRentalForm.end && rental.dateEnd > searchRentalForm.end)
+    fun setCarActive(id: Long, active: Boolean): CarEntity {
+        var car = carService.getById(id)
+        var rentals = repository.findByCar(car)
+        val currentDate = LocalDate.now()
+        for(rental : RentalEntity in rentals) {
+            if(currentDate <= rental.dateEnd) {
+                throw ResponseStatusException(HttpStatus.NOT_ACCEPTABLE)
+            }
+        }
+        return carService.setActive(id, active)
+    }
+
+    fun isRentedAtDate(rental: RentalEntity, dateRange: DateRangeModel): Boolean =
+        (rental.dateStart <= dateRange.start && rental.dateEnd >= dateRange.end) ||
+                (rental.dateStart >= dateRange.start && rental.dateEnd <= dateRange.end) ||
+                (rental.dateStart <= dateRange.start && rental.dateEnd >= dateRange.start) ||
+                (rental.dateStart <= dateRange.end && rental.dateEnd >= dateRange.end)
 }
