ca224d79e4df6b9ecd89c27601db4c855faea59d
Wolf Dániel <wolf.dani9@gmail.com>
2023-06-22 13:11:02 +0200

    added field to represent a car's active status, and a method to calculate the price for x days

--- src/main/kotlin/hu/danielwolf/udemxcarrental/models/CarEntity.kt
+++ src/main/kotlin/hu/danielwolf/udemxcarrental/models/CarEntity.kt
@@ -10,6 +10,9 @@ data class CarEntity (
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
     var licensePlateNumber: String,
     var dailyPrice: Long,
+    var active: Boolean? = true,
     @Lob var image: ByteArray? = null,
     @OneToMany @JsonIgnore var rentals: List<RentalEntity>? = null
-)
+) {
+    fun getPriceForDays(days: Long) = days * dailyPrice
+}
