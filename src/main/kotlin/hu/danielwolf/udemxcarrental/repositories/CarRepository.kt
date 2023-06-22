f5f513a9585e51e4cafc94303f8d71e681f9d120
Wolf Dániel <wolf.dani9@gmail.com>
2023-06-22 13:22:28 +0200

    added repository function to get all active cars

--- src/main/kotlin/hu/danielwolf/udemxcarrental/repositories/CarRepository.kt
+++ src/main/kotlin/hu/danielwolf/udemxcarrental/repositories/CarRepository.kt
@@ -3,6 +3,10 @@ package hu.danielwolf.udemxcarrental.repositories
 import hu.danielwolf.udemxcarrental.models.CarEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
-interface CarRepository : JpaRepository<CarEntity, Long>
+interface CarRepository : JpaRepository<CarEntity, Long> {
+    @Transactional
+    fun findByActiveEquals(active: Boolean): List<CarEntity>
+}
