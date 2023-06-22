3b8ae71726e892ca5cb66f5c3bdb7da0af2be5fe
Wolf Dániel <wolf.dani9@gmail.com>
2023-06-22 14:07:56 +0200

    added function to rental repository to find all rentals belonging to a car

--- src/main/kotlin/hu/danielwolf/udemxcarrental/repositories/RentalRepository.kt
+++ src/main/kotlin/hu/danielwolf/udemxcarrental/repositories/RentalRepository.kt
@@ -1,8 +1,13 @@
 package hu.danielwolf.udemxcarrental.repositories
 
+import hu.danielwolf.udemxcarrental.models.CarEntity
 import hu.danielwolf.udemxcarrental.models.RentalEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
-interface RentalRepository : JpaRepository<RentalEntity, Long>
+interface RentalRepository : JpaRepository<RentalEntity, Long> {
+    @Transactional
+    fun findByCar(car: CarEntity): List<RentalEntity>
+}
